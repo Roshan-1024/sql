5bad82095bb08f26956ff1e1bbf65a765d0ccd29
Roshan-1024 <roshanpradhan1024@gmail.com>
2025-02-25 05:10:33 +0530

    create a db with a table structure

--- src/main.py
+++ src/main.py
@@ -1,1 +1,31 @@
-print("Hello world")
+import mysql.connector
+
+# Connect to MySQL (XAMPP)
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",  # Default XAMPP MySQL user
+    password="",  # Default is empty (no password)
+)
+
+if conn.is_connected():
+    print("connection successful")
+
+cursor = conn.cursor()
+
+command = """
+CREATE DATABASE mydb;
+USE mydb;
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	age INT
+);
+"""
+cursor.execute(command)
+for table in cursor.fetchall():
+    print(table)
+
+cursor.close()
+conn.close()
+
